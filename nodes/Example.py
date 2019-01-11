537d72759e220148349d10895a17941fa3bd8cc8
uriahrokach <uriahrokach@gmail.com>
2019-01-11 20:51:59 +0200

    Changed Example code

--- nodes/Example.py
+++ nodes/Example.py
@@ -2,6 +2,7 @@ import nodes.Node as Node
 
 
 def main():
+    # This is a data that represents a flower.
     data = ((3, 1.5, 1),
             (2, 1, 0),
             (4, 1.5, 1),
@@ -12,10 +13,13 @@ def main():
             (1, 1, 0))
 
     neuron = Node.Node(data_list=data)
-    neuron.train(lrn_rate=0.2, iterations=50000)
-    print(neuron.weights)
-    print(neuron.bias)
-    print(neuron.data_list)
+    neuron.train(lrn_rate=0.2, iterations=1000000)
+    while True:
+        result = int(100 * neuron.run_node())
+        if result > 50:
+            print("the node is sure in "+str(result)+"% that this is a red flower.")
+        else:
+            print("the node is sure in " + str(100 - result) + "% that this is a blue flower.")
 
 
 main()
